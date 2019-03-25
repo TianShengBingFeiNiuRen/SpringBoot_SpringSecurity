51db2be9416a63c71b08babd569f3cb797d6383a
天生并非牛人 <34623775+TianShengBingFeiNiuRen@users.noreply.github.com>
2019-03-25 13:14:07 +0800

    Update RoleService.java

--- src/main/java/com/nonce/restsecurity/service/RoleService.java
+++ src/main/java/com/nonce/restsecurity/service/RoleService.java
@@ -1,8 +1,10 @@
 package com.nonce.restsecurity.service;
 
 import com.nonce.restsecurity.dao.AuthorityUserRepository;
+import com.nonce.restsecurity.util.SecurityResponse;
 import com.nonce.restsecurity.util.TimeUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
@@ -79,7 +81,7 @@ public class RoleService {
     /**
      * 获取所有的角色信息 (分页加模糊查询)
      */
-    public List<Map<String, Object>> findAllRoleInfo(String pageNum, String pageSize, String roleNameCN) {
+    public List<Map<String, Object>> findAllRoleInfo(String pageNum, String pageSize, String roleNameCN, SecurityResponse securityResponse) {
         int rowNum = Integer.parseInt(pageNum);
         int size = Integer.parseInt(pageSize);
         int row = (rowNum - 1) * size;
@@ -93,26 +95,47 @@ public class RoleService {
             List<Map<String, Object>> rootMenuInfoByRoleId = authorityUserRepository.findRootMenuInfoByRoleId(roleId);
             rootMenuInfoByRoleId.forEach(rootMenuInfo -> {
                 int id = (int) rootMenuInfo.get("id");
-                List<Map<String, Object>> children = authorityUserRepository.findMenuInfoByParentId(id);
-                Map<String, Object> map = new HashMap<>();
-                map.put("id", id);
-                map.put("url", rootMenuInfo.get("url"));
-                map.put("menuName", rootMenuInfo.get("menuName"));
-                map.put("parentId", rootMenuInfo.get("parentId"));
-                map.put("urlPre", rootMenuInfo.get("urlPre"));
-                map.put("children", children);
-                menuInfoList.add(map);
+                if (id == 0) {
+                    List<Map<String, Object>> rootMenuInfoList = authorityUserRepository.findRootMenuInfo();
+                    rootMenuInfoList.forEach(rootMenu -> {
+                        int rootId = (int) rootMenu.get("id");
+                        List<Map<String, Object>> children = authorityUserRepository.findMenuInfoByParentId(rootId);
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("id", rootId);
+                        map.put("url", rootMenu.get("url"));
+                        map.put("menuName", rootMenu.get("menuName"));
+                        map.put("parentId", rootMenu.get("parentId"));
+                        map.put("urlPre", rootMenu.get("urlPre"));
+                        map.put("children", children);
+                        menuInfoList.add(map);
+                    });
+                } else {
+                    List<Map<String, Object>> children = authorityUserRepository.findMenuInfoByParentId(id);
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("id", id);
+                    map.put("url", rootMenuInfo.get("url"));
+                    map.put("menuName", rootMenuInfo.get("menuName"));
+                    map.put("parentId", rootMenuInfo.get("parentId"));
+                    map.put("urlPre", rootMenuInfo.get("urlPre"));
+                    map.put("children", children);
+                    menuInfoList.add(map);
+                }
             });
-            List<Map<String, Object>> notRootMenuInfoByRoleId = authorityUserRepository.findNotRootMenuInfoByRoleId(roleId);
-            notRootMenuInfoByRoleId.forEach(rootMenuInfo -> {
-                Map<String, Object> map = new HashMap<>();
-                map.put("id", rootMenuInfo.get("id"));
-                map.put("url", rootMenuInfo.get("url"));
-                map.put("menuName", rootMenuInfo.get("menuName"));
-                map.put("parentId", rootMenuInfo.get("parentId"));
-                map.put("urlPre", rootMenuInfo.get("urlPre"));
-                menuInfoList.add(map);
-            });
+            List<Integer> rootMenuIdOfPartialPermissionByRoleId = authorityUserRepository.findRootMenuIdOfPartialPermissionByRoleId(roleId);
+            if (!ObjectUtils.isEmpty(rootMenuIdOfPartialPermissionByRoleId)) {
+                rootMenuIdOfPartialPermissionByRoleId.forEach(menuId -> {
+                    Map<String, Object> rootMenuInfo = authorityUserRepository.findMenuInfoByMenuId(menuId);
+                    List<Map<String, Object>> children = authorityUserRepository.findChildrenMenuInfoByRoleIdAndParentId(roleId, menuId);
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("id", menuId);
+                    map.put("url", rootMenuInfo.get("url"));
+                    map.put("menuName", rootMenuInfo.get("menuName"));
+                    map.put("parentId", rootMenuInfo.get("parentId"));
+                    map.put("urlPre", rootMenuInfo.get("urlPre"));
+                    map.put("children", children);
+                    menuInfoList.add(map);
+                });
+            }
             Map<String, Object> map = new HashMap<>();
             map.put("id", roleId);
             map.put("roleName", roleInfo.get("roleName"));
@@ -121,13 +144,19 @@ public class RoleService {
             map.put("menuList", menuInfoList);
             list.add(map);
         }
+        int roleInfoSize = authorityUserRepository.findAllRoleInfoSize(name);
+        securityResponse.setSuccess(true);
+        securityResponse.setCode("1");
+        securityResponse.setMessage("Find all role success!!");
+        securityResponse.setData(list);
+        securityResponse.setTotal(roleInfoSize);
         return list;
     }
 
     /**
      * 修改的角色名是否存在 不存在 返回true
      */
-    public boolean isNotExistenceOfUpdateRoleName(String id, String roleName){
+    public boolean isNotExistenceOfUpdateRoleName(String id, String roleName) {
         int roleId = Integer.parseInt(id);
         int count = authorityUserRepository.isNotExistenceOfUpdateRoleName(roleId, roleName);
         return count == 0;
