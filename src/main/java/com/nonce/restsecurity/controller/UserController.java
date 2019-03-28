111cafe05da329822004479ba49de5ae2b8c2a0e
天生并非牛人 <34623775+TianShengBingFeiNiuRen@users.noreply.github.com>
2019-03-28 10:31:50 +0800

    Update UserController.java

--- src/main/java/com/nonce/restsecurity/controller/UserController.java
+++ src/main/java/com/nonce/restsecurity/controller/UserController.java
@@ -88,7 +88,7 @@ public class UserController {
     @PostMapping(value = "/user/update")
     public SecurityResponse update(String id, String nickname, String username, String password, String email, String phone, String validTime, String remark) {
         try {
-            if (!ObjectUtils.isEmpty(id) && !ObjectUtils.isEmpty(nickname) && !ObjectUtils.isEmpty(username) && !ObjectUtils.isEmpty(password)) {
+            if (!ObjectUtils.isEmpty(id) && !ObjectUtils.isEmpty(nickname) && !ObjectUtils.isEmpty(username) && ((ObjectUtils.isEmpty(validTime) && !ObjectUtils.isEmpty(password)) || (!ObjectUtils.isEmpty(validTime) && ObjectUtils.isEmpty(password)))) {
                 boolean notExistenceOfUpdateUsername = userService.isNotExistenceOfUpdateUsername(id, username);
                 if (notExistenceOfUpdateUsername) {
                     userService.updateUserInfo(id, nickname, username, password, email, phone, validTime, remark);
