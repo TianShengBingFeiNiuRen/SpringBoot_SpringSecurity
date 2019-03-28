c94c83c4b2a7085a790d3444aec20a0bde31a65f
天生并非牛人 <34623775+TianShengBingFeiNiuRen@users.noreply.github.com>
2019-03-28 18:45:37 +0800

    Update PersonalController.java

--- src/main/java/com/nonce/restsecurity/controller/PersonalController.java
+++ src/main/java/com/nonce/restsecurity/controller/PersonalController.java
@@ -2,6 +2,9 @@ package com.nonce.restsecurity.controller;
 
 import com.nonce.restsecurity.config.UrlResponse;
 import com.nonce.restsecurity.service.UserService;
+import com.nonce.restsecurity.util.SecurityResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +19,12 @@ import java.util.Map;
  * <p>
  * 个人中心
  */
+@SuppressWarnings("Duplicates")
 @RestController
 @RequestMapping(value = "/common")
 public class PersonalController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PersonalController.class);
 
     @Resource
     private UserService userService;
@@ -44,5 +50,28 @@ public class PersonalController {
             response.setData("Incomplete information!!");
         }
         return response;
+    }
+
+    /**
+     * 修改个人信息
+     */
+    @PostMapping(value = "/personalUserInfo/update")
+    public SecurityResponse update(String id, String nickname, String username, String password, String email, String phone, String validTime, String remark) {
+        try {
+            if (!ObjectUtils.isEmpty(id) && !ObjectUtils.isEmpty(nickname) && !ObjectUtils.isEmpty(username) && ((ObjectUtils.isEmpty(validTime) && !ObjectUtils.isEmpty(password)) || (!ObjectUtils.isEmpty(validTime) && ObjectUtils.isEmpty(password)))) {
+                boolean notExistenceOfUpdateUsername = userService.isNotExistenceOfUpdateUsername(id, username);
+                if (notExistenceOfUpdateUsername) {
+                    userService.updateUserInfo(id, nickname, username, password, email, phone, validTime, remark);
+                    return new SecurityResponse(true, "1", "Update userInfo success!!", "username: " + nickname + " update success!!");
+                } else {
+                    return new SecurityResponse(false, "-1", "Update userInfo failure!!", "username: " + username + " already exists!!");
+                }
+            } else {
+                return new SecurityResponse(false, "-1", "Incomplete information!!", "Incomplete information!!");
+            }
+        } catch (Exception e) {
+            LOG.error("updateUserInfo failure!! error={}", e.getMessage());
+            return new SecurityResponse(false, "-1", "Update userInfo failure!!", "username: " + nickname + " update failure!!");
+        }
     }
 }
